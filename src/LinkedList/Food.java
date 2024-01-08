dddbcef05e0804f41f9b0e1d8aa355bc50c95d29
Dinara Amangazieva <ahmedovadin@gmail.com>
2024-01-08 23:40:13 +0600

    LinkedList peekFirst / peekLast, peekLast / pollLast, toArray methods

--- src/LinkedList/Food.java
+++ src/LinkedList/Food.java
@@ -25,6 +25,30 @@ public class Food {
         food.addFirst(sushi);
         food.addLast(beshbarmak);
         System.out.println(food);
+
+        System.out.println(food.peekFirst());
+        System.out.println(food.peekLast());
+
+        LinkedList<Food> vegetables = new LinkedList<>();
+        Food tomato = new Food("Tomato");
+        Food potato = new Food("Potato");
+        Food cabbage = new Food("Cabbage");
+        Food carrot = new Food("Carrot");
+
+        vegetables.add(tomato);
+        vegetables.add(potato);
+        vegetables.add(cabbage);
+        vegetables.add(carrot);
+
+        Food[] foodArray = vegetables.toArray(new Food[4]);
+        System.out.println(Arrays.toString(foodArray));
+
+        System.out.println(vegetables.peekFirst());
+        System.out.println(vegetables.peekLast());
+
+        System.out.println(vegetables.peekLast());
+        System.out.println(vegetables.pollLast());
+        System.out.println(vegetables);
     }
 
     @Override
